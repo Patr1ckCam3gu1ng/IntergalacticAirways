73bde6255ab4f8c2e34dec71f4e1e5996f150f42
Patr Cam <Patr Cam@DESKTOP-QJS7CC0>
2021-01-28 12:18:29 +0800

    Initial implementation: Added project files

---
+++ IntergalacticAirways.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IntergalacticAirways.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ IntergalacticAirways.DAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntergalacticAirways.DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
